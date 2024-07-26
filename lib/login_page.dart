5449adda6e6f4f8f992ab430d3b49625836a7e66
hsyntinaztepe <tinaztepe97@gmail.com>
2024-07-26 22:01:38 +0300

    login_page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,32 +14,43 @@ class LoginPage extends StatelessWidget {
         },
         child: Icon(Icons.add), // Added icon to FloatingActionButton
       ),
-      body: Column(
-        children: [
-          Text(
-            'Let\'s sign you in!',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.brown,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 0.5,
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'Let\'s sign you in!',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.brown,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.5,
+              ),
             ),
-          ),
-          Text(
-            'Welcome back! \nYou\'ve been missed!',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
+            Text(
+              'Welcome back! \nYou\'ve been missed!',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
             ),
-          ),
-          Image.network(
-            "https://upload.wikimedia.org/wikipedia/tr/a/a3/Jerry_Fare.png",
-          ),
-          ElevatedButton(
-              onPressed: null,
-              child: Text("Click Me!",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300))),
-        ],
+            Image.network(
+              "https://upload.wikimedia.org/wikipedia/tr/a/a3/Jerry_Fare.png",
+            ),
+
+            TextField(
+              controller: UserNameController,
+              decoration: InputDecoration(
+                hintText: "Add Your Username",
+                hintStyle:TextStyle(color: Colors.blueGrey),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: null,
+                child: Text("Click Me!",
+                    style:
+                        TextStyle(fontSize: 30, fontWeight: FontWeight.w300))),
+          ],
+        ),
       ),
     );
   }
